f37047fd98cbbf687e9fefdcfdeca9f289b0b08b
Paul <paul.jo.thompson@gmail.com>
2016-01-29 08:07:09 -0800

    dom scripting events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,28 @@
+var pointsArray = Array.from(document.getElementsByClassName('point'));
 
-var revealPoint = function(points){    
- var points = document.getElementsByClassName('point');
+var animatePoints = function(points){ 
+
+    pointsElements.forEach(function (element) {
+
+    element.style.opacity = 1;
+    element.style.transform = "scaleX(1) translateY(0)";
+    element.style.msTransform = "scaleX(1) translateY(0)";
+    element.style.WebkitTransform = "scaleX(1) translateY(0)";
+    });
+}; 
+
+window.onload = function() {
     
-    for(i = 0; i < points.length; i++){  
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].style.msTransform = "scaleX(1) translateY(0)";
-    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    if(window.innerHeight > 950){
+     animatePoints(pointsArray);
     }
- }; 
-
-revealPoint();
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+    });
+}
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+var pointsElements = Array.from(document.getElementsByClassName('point'));
+
+pointsElements.forEach(function (element) {
+    console.log(element);
+});
